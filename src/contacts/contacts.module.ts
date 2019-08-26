9ff5d8f67e3509e825f0c94264137130796718ff
Renan Pallin <renanpallin@gmail.com>
2019-08-26 11:40:23 -0300

    nest generate controller contacts

--- src/contacts/contacts.module.ts
+++ src/contacts/contacts.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ContactsService } from './contacts.service';
+import { ContactsController } from './contacts.controller';
 
 @Module({
-  providers: [ContactsService]
+  providers: [ContactsService],
+  controllers: [ContactsController]
 })
 export class ContactsModule {}
